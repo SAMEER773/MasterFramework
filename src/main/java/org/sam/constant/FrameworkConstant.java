a3f8563cfd7222b1b804a7831d1bbf085dc2bd32
Sameer <ssameerrathod@outlook.com>
2023-05-14 09:19:40 +0530

    my second commit

--- src/main/java/org/sam/constant/FrameworkConstant.java
+++ src/main/java/org/sam/constant/FrameworkConstant.java
@@ -1,10 +1,10 @@
-package org.sam.constatnt;
+package org.sam.constant;
 
 // if we dont want to allow people to extend this  class make class as final
 //if we want to restrict to people for creating object create private constructor
-public final class FrameworkConstatnt {
+public final class FrameworkConstant {
 
-	private FrameworkConstatnt() {
+	private FrameworkConstant() {
 
 	}
 
--- src/main/java/org/sam/driver/Driver.java
+++ src/main/java/org/sam/driver/Driver.java
@@ -3,7 +3,7 @@ package org.sam.driver;
 import java.util.Objects;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.sam.constatnt.FrameworkConstatnt;
+import org.sam.constant.FrameworkConstant;
 import org.sam.utils.ReadPropertyFile;
 
 public class Driver {
@@ -13,7 +13,7 @@ public class Driver {
 		if (Objects.isNull(DriverManager.getDriver())) { // to avoid to call this method if some one call this method
 															// its not affect to
 			// our frame work
-			System.setProperty("webdriver.chrome.driver", FrameworkConstatnt.CHROMEDRIVERPATH());
+			System.setProperty("webdriver.chrome.driver", FrameworkConstant.CHROMEDRIVERPATH());
 
 			DriverManager.setDriver(new ChromeDriver());
 			DriverManager.getDriver().get(ReadPropertyFile.getValue("URL"));
